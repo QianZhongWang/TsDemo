8104b8e0bbd069c0c8fa2bc3913885e3d6537d96
QianZhongWang <1870271825@qq.com>
2020-10-12 22:28:34 +0800

    ✨feat(all): 处理错误信息

--- axios/examples/server.js
+++ axios/examples/server.js
@@ -21,24 +21,24 @@ app.use(webpackHotMiddleware(compiler))
 app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 
 const router = express.Router()
 
-router.get('/simple/get', function(req, res) {
+router.get('/simple/get', function (req, res) {
   res.json({
     msg: `hello world`
   })
 })
-router.get('/base/get', function(req, res) {
+router.get('/base/get', function (req, res) {
   res.json(req.query)
 })
 
-router.post('/base/post', function(req, res) {
+router.post('/base/post', function (req, res) {
   res.json(req.body)
 })
 
-router.post('/base/buffer', function(req, res) {
+router.post('/base/buffer', function (req, res) {
   let msg = []
   req.on('data', (chunk) => {
     if (chunk) {
@@ -49,6 +49,25 @@ router.post('/base/buffer', function(req, res) {
     let buf = Buffer.concat(msg)
     res.json(buf.toJSON())
   })
+})
+
+router.get('/error/get', function (req, res) {
+  if (Math.random() > 0.5) {
+    res.json({
+      msg: `hello world`
+    })
+  } else {
+    res.status(500)
+    res.end()
+  }
+})
+
+router.get('/error/timeout', function (req, res) {
+  setTimeout(() => {
+    res.json({
+      msg: `hello world`
+    })
+  }, 3000)
 })
 app.use(router)
 
