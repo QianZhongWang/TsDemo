b39109d31c3e62225f669be06593c20405c914da
QIANZW <1870271825@gqq.com>
2020-10-13 10:06:56 +0800

    ✨feat(all): 响应数据支持泛型

--- axios/src/core/Axios.ts
+++ axios/src/core/Axios.ts
@@ -1,8 +1,16 @@
-import { AxiosPromise, AxiosRequestConfig, Method } from '../types'
+import {AxiosPromise, AxiosRequestConfig, Method} from '../types'
 import dispatchRequest from './dispatchRequest'
 
 export default class Axios {
-  request(config: AxiosRequestConfig): AxiosPromise {
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
 
@@ -35,10 +43,10 @@ export default class Axios {
   }
 
   _requestMethodWithoutData(method: Method, url: string, config?: AxiosRequestConfig) {
-    return this.request(Object.assign(config || {}, { method, url }))
+    return this.request(Object.assign(config || {}, {method, url}))
   }
 
   _requestMethodWithData(method: Method, url: string, data?: any, config?: AxiosRequestConfig) {
-    return this.request(Object.assign(config || {}, { method, url, data }))
+    return this.request(Object.assign(config || {}, {method, url, data}))
   }
 }
